7c26e74662726c6f4f208b25b2bab16a28fe5db7
Raj Sharma <138351100+rajsha10@users.noreply.github.com>
2024-01-18 22:23:45 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,49 +1,47 @@
 var front = document.querySelector('.face-front');
 var back = document.querySelector('.face-back');
 var flip = document.querySelector('.book-content');
-var uno = document.querySelectorAll('.book');
-var portada = document.querySelectorAll('#portada');
+var cards = document.querySelectorAll('.book'); 
+var cover = document.querySelectorAll('#cover');
 
 var contZindex = 2;
 var customZindex = 1;
 
-for (var i = 0; i < uno.length; i++) {
-	uno[i].style.zIndex = customZindex;
-	customZindex--;
+for (var i = 0; i < cards.length; i++) {
+    cards[i].style.zIndex = customZindex;
+    customZindex--;
 
-	uno[i].addEventListener('click', function(e){
+    cards[i].addEventListener('click', function (e) {
 
-		var tgt = e.target;
-		var unoThis = this;
+        var tgt = e.target;
+        var cardThis = this; 
+        cardThis.style.zIndex = contZindex;
+        contZindex++;
 
-		unoThis.style.zIndex = contZindex;
-		contZindex++;
+        if (tgt.getAttribute('class') == 'face-front') {
+            cardThis.style.zIndex = contZindex;
+            contZindex += 20;
+            setTimeout(function () {
+                cardThis.style.transform = 'rotateY(-180deg)';
+            }, 500);
+        }
+        if (tgt.getAttribute("class") == 'face-back') {
+            cardThis.style.zIndex = contZindex;
+            contZindex += 20;
 
-		if (tgt.getAttribute('class') == 'face-front') {
-			unoThis.style.zIndex = contZindex;
-			contZindex +=20;
-			setTimeout(function(){
-				unoThis.style.transform = 'rotateY(-180deg)';
-			}, 500);
-		}
-		if (tgt.getAttribute("class") == 'face-back') {
-			unoThis.style.zIndex = contZindex;
-			contZindex +=20;
+            setTimeout(function () {
+                cardThis.style.transform = 'rotateY(0deg)';
+            }, 500);
+        }
 
-			setTimeout(function(){
-				unoThis.style.transform = 'rotateY(0deg)';
-			}, 500);
-		}
+        if (tgt.getAttribute('id') == 'cover') {
+            flip.classList.remove("trnsf-reset");
+            flip.classList.add("trnsf");
+        }
+        if (tgt.getAttribute('id') == 'trsf') {
+            flip.classList.remove("trnsf");
+            flip.classList.add("trnsf-reset");
+        }
 
-		if (tgt.getAttribute('id') == 'portada') {
-			flip.classList.remove("trnsf-reset");
-			flip.classList.add("trnsf");
-		}
-		if (tgt.getAttribute('id') == 'trsf') {
-			flip.classList.remove("trnsf");
-			flip.classList.add("trnsf-reset");
-		}
-
-	});
+    });
 }
-
