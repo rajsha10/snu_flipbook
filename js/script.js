fd87b8f07b3859afd2388e329b5993e0c7768dbb
rajsha10 <rajsha8617@gmail.com>
2024-01-19 22:46:07 +0530

    updating code with changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 var front = document.querySelector('.face-front');
 var back = document.querySelector('.face-back');
 var flip = document.querySelector('.book-content');
-var cards = document.querySelectorAll('.book'); 
-var cover = document.querySelectorAll('#cover');
+var cards = document.querySelectorAll('.book');
+var cover = document.querySelectorAll('#fcover');
+var container = document.querySelector('.book-content');
 
 var contZindex = 2;
 var customZindex = 1;
@@ -12,9 +13,8 @@ for (var i = 0; i < cards.length; i++) {
     customZindex--;
 
     cards[i].addEventListener('click', function (e) {
-
         var tgt = e.target;
-        var cardThis = this; 
+        var cardThis = this;
         cardThis.style.zIndex = contZindex;
         contZindex++;
 
@@ -25,7 +25,7 @@ for (var i = 0; i < cards.length; i++) {
                 cardThis.style.transform = 'rotateY(-180deg)';
             }, 500);
         }
-        if (tgt.getAttribute("class") == 'face-back') {
+        if (tgt.getAttribute('class') == 'face-back') {
             cardThis.style.zIndex = contZindex;
             contZindex += 20;
 
@@ -43,5 +43,11 @@ for (var i = 0; i < cards.length; i++) {
             flip.classList.add("trnsf-reset");
         }
 
+        //last slide after book close
+        if (contZindex > cards.length * 20) {
+            setTimeout(function () {
+                container.classList.add("slide-to-center");
+            }, 2000);
+        }
     });
 }
