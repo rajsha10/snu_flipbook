0df9ae8c00ab567a3ab10cddee1181927ffa8d83
Arunima Chatterjee <92734728+TheHappyBaloney@users.noreply.github.com>
2024-01-20 15:54:28 +0000

    deleted tfidf file

--- Python/singleclassrecommender.py
+++ Python/singleclassrecommender.py
@@ -13,6 +13,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import joblib
 import random
+import requests
 
 class BookRecommendationSystem:
     def __init__(self):
