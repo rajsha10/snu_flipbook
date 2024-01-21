9fb4aa7f204b80fab1e400421a2498e877784870
Arunima Chatterjee <92734728+TheHappyBaloney@users.noreply.github.com>
2024-01-21 14:04:16 +0000

    Changes

--- Python/input.py
+++ Python/input.py
@@ -1,4 +1,3 @@
 user_favorite_books = input("Enter your three favorite books (comma-separated): ")
 user_favorite_authors = input("Enter your three favorite authors (comma-separated): ")
 user_topics_of_interest = input("Enter your three topics of interest (comma-separated): ")
-
