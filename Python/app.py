c47e5071195a45ad0926e355779cc112e3b1b2d0
Soumedhik Bharati <113777577+Soumedhik@users.noreply.github.com>
2024-01-21 07:47:54 +0000

    changes made to app.py

--- Python/app.py
+++ Python/app.py
@@ -9,6 +9,3 @@ model = genai.GenerativeModel("gemini-pro", generation_config=generation_config)
 
 user_input_prompt = f"Given the favorite books {user_favorite_books} in the style of the author {user_favorite_authors}, recommend the top 5 closest books. Consider topics of interest: {user_topics_of_interest}."
 gemini_response = model.generate_content(user_input_prompt)
-
-print("Response:")
-print(gemini_response.text)
