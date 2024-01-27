59a2179925fd7d094aef561aa7464a4c3b148de8
manjeetsharma0796 <manjeetsharma0796@gmail.com>
2024-01-27 18:26:25 +0530

    modify: using script in tv and updated deprecated way

--- src/storeUserDetails.js
+++ src/storeUserDetails.js
@@ -5,7 +5,7 @@ const insertDetails = (req,res) => {
     console.log(req.body);
     const userDetails = req.body;
     insertUser(userDetails);
-    res.send(200);
+    res.sendStatus(200);
 }
 
 module.exports = {insertDetails};
