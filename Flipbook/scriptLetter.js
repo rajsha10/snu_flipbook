aff9d220862c231bd75d600e561f79785722f5f2
manjeetsharma0796 <manjeetsharma0796@gmail.com>
2024-01-30 19:48:56 +0530

    add: caveat font, update regx

--- Flipbook/scriptLetter.js
+++ Flipbook/scriptLetter.js
@@ -3,7 +3,7 @@ window.onload = () => {
     fetch('/recommend')
     .then((req) => req.text())
     .then((recomendation) => {
-        const reformedList = recomendation.replaceAll('\r\n\r\n', "<br>")
+        const reformedList = recomendation.replaceAll('\r\n', "<br>")
         .replaceAll(/\.\s*"/g, "<b>.  ").replaceAll('\"', "</b>")
         recommendPanel.innerHTML = reformedList
     })
